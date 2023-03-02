f0da93c8de251c0919b75708b143c417c91799a7
Rajat Jindal <rajatjindal83@gmail.com>
2023-03-02 16:54:44 +0530

    try error

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,41 @@ macro_rules! assert {
 }
 
 #[macro_export]
+macro_rules! assert_eq2 {
+    ($left:expr, $right:expr $(,)?) => {{
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if !(*left_val == *right_val) {
+                    Err($crate::ErrorBuilder::new()
+                        .file(file!())
+                        .line(line!())
+                        .column(column!()))
+                } else {
+                    Ok(())
+                }
+            }
+        }
+    }};
+    ($left:expr, $right:expr, $($arg:tt)+) => {{
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if !(*left_val == *right_val) {
+                    Err($crate::ErrorBuilder::new()
+                        .file(file!())
+                        .line(line!())
+                        .column(column!())
+                        .message(format!($($arg)+))
+                        .build())
+                } else {
+                    Ok(())
+                }
+            }
+        }
+    }};
+}
+
+
+#[macro_export]
 macro_rules! assert_eq {
     ($left:expr, $right:expr $(,)?) => {{
         match (&$left, &$right) {
